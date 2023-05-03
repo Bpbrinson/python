37d09617e477842c0ed62320c6bf473f302fafd9
Brandon Brinson <Brandon.Brinson@avantorsciences.com>
2023-05-03 13:21:51 -0400

    removed unnecessary php code

--- connection.php
+++ connection.php
@@ -6,10 +6,4 @@ $password = "PfjRn7jk7598";
 $dbname = "finance";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$conn->close();
 ?>
